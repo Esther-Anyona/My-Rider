d007fe1a8082e3941ef678e62ee0f4ef18300266
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-16 06:37:56 +0300

    Basic app structure in place

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from . import auth, main
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    # Blueprints    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ start.sh
@@ -0,0 +1,2 @@
+export SECRET_KEY=b'\xe5\xf8\xaa\x13r\xf3=\xa1W@,lC\x1e"'
+python3 manage.py server
