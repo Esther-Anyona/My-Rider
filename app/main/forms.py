c531de6001989a1fc5d415dc91175f2d0c10a141
DKorir <dominic.korir@student.moringaschool.com>
2022-02-16 21:23:52 +0300

    create wtf forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,14 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,SearchField
+from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import InputRequired
 
-class SearchForm(FlaskForm):
-    search = SearchField('search location', validators=[InputRequired()])
-    submit = SubmitField('Submit')
 
 class UsrForm(FlaskForm):
-    pick_up = SearchField('search location', validators=[InputRequired()])
-    Destination = SearchField('search location', validators=[InputRequired()])
-    submit = SubmitField('Submit')
-    r_now = SearchField('Request Now', validators=[InputRequired()])
-    s_later = SearchField('Request later', validators=[InputRequired()])
+    pick_up = StringField('Enter location', validators=[InputRequired()])
+    Destination = StringField('Enter Destination', validators=[InputRequired()])
+    r_now = StringField('Request Now', validators=[InputRequired()])
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,27 @@
 from . import main
 from flask import render_template,request,redirect,url_for
-from .forms import SearchForm,UsrForm
+from .forms import UsrForm
+from app.main import forms
+
+
 @main.route('/')
 def index():
   return '<h1>Welcome to Ride Link<h1>'
 
-@main.route('/domie_user')
-def domie_user():
-  return render_template('domie-user.html')
+@main.route('/usr', )
+def usr():
+  loc=request.args.get('loc')
+
+
+  return render_template('usr.html')
+@main.route('/usrform', methods=['POST','GET'])
+def usrform():
+  form=UsrForm()
+  if request.method == 'POST':
+    pick_up = request.form.get('pick_up')
+    Destination = request.form.get('Destination')
+    r_now = request.form.get('r_now')
+
+    
+
+  return render_template('usr.html',form=form)
