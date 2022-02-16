0b4838450a2db1d9d2a430f8d96c363bcdfbf8f0
DKorir <dominic.korir@student.moringaschool.com>
2022-02-16 14:58:35 +0300

    create searc form for location

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,9 @@
 from . import main
-
+from flask import render_template,request,redirect,url_for
 @main.route('/')
 def index():
   return '<h1>Welcome to Ride Link<h1>'
+
+@main.route('/domie_user')
+def domie_user():
+  return render_template('domie-user.html')
